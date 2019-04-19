f1757c1184cfd01865dcfdd9dac9b9eac48c3dbc
Fernand <moudio.fernand@yandex.ru>
2019-04-19 19:05:45 +0300

    search input

--- memory/src/Components/headers/description.js
+++ memory/src/Components/headers/description.js
@@ -3,12 +3,14 @@ class Description extends React.Component {
     constructor() {
         super()
         this.state = {
-
+            logoName: "S",
+            href: "#"
         }
     }
     render() {
         return (
         <div className="header-container__title">
+            <a href={this.state.href}><span className="header-container__title__logoName">{this.state.logoName}</span></a>
             <span className="header-container__title__secondTitle">{this.props.shopHeaderTitle}</span>
             <span className="header-container__title__description">{this.props.shopSmallDescription}</span>
         </div>)
---
+++ memory/src/Components/headers/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div>
+                <form action="" method="get">
+                    <input type="text"/>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        )
+    }
+}
