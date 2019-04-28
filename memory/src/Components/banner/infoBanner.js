ffe5f57e018c87689219a756a8491eac6a539dff
Fernand Moudio <fernandmoudio1@gmail.com>
2019-04-28 14:58:56 +0300

    banner  class

--- memory/src/Components/banner/infoBanner.js
+++ memory/src/Components/banner/infoBanner.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../Styles_Components/PromoBanner.scss';
 
-const Info = (props) => {
-    return (
-    <div className="banner__container__title">
-        <span>{props.shopTitle}</span>
-    </div>
-    );
+class Info extends React.Component {
+    render() {
+        return(
+            <div className="banner__container__title">
+                <span>{this.props.shopTitle}</span>
+            </div>
+        );
+    }
 }
 
 export default Info
