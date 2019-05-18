3dd89ded27041560fd1209a83a479c84163e7daa
Fernand Moudio <fernandmoudio1@gmail.com>
2019-05-18 14:34:38 +0300

    about routes

--- memory/src/App.js
+++ memory/src/App.js
@@ -3,25 +3,63 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import MainContent from './Components/MainContent';
 import Footer from './Components/Footer';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 
-class App extends Component {
+const history = createBrowserHistory();
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+const Contacts = () => (
+  <div>
+    <h2>Contact</h2>
+  </div>
+)
+
+class Navigation extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <div className="title">
-          <h1>Fernand Moudio</h1>
-        </div>
+    return(
+      <BrowserRouter history={history}>
         <div>
-          <p>i am a web fronted debelopper</p>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contacts">Contacts</Link></li>
+          </ul>
+          <hr/>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contacts" component={Contacts}/>
         </div>
-        <div className="App-Section">
-          <Navbar />
-          <MainContent />
-        </div>
-        <Footer />
-      </div>
-    );
+      </BrowserRouter>
+    )
   }
 }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="title">
+//           <h1>Fernand Moudio</h1>
+//         </div>
+//         <div>
+//           <p>i am a web fronted debelopper</p>
+//         </div>
+//         <div className="App-Section">
+//           <Navbar />
+//           <MainContent />
+//         </div>
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+export default Navigation;
