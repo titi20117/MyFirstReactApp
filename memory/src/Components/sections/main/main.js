31dbbe909402a56f34d1bd09650c24a28b71b1fe
Fernand Moudio <fernandmoudio1@gmail.com>
2019-05-31 06:37:11 +0300

    fetch data picture from data

--- memory/src/Components/sections/main/main.js
+++ memory/src/Components/sections/main/main.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import '../../../Styles_Components/Section/main.scss';
 import ProductsData from './productData';
-// import '../productsImages/';
+// import productsImages from '../productsImages/adidas-porsche-design-athletic.jpg';
 
 class Main extends React.Component {
     constructor(){
         super();
         this.state=({
-            db: []
+            db: [] 
         });
         this.showProducts();
     }
@@ -26,46 +26,8 @@ class Main extends React.Component {
     render() {
         return(
             <div className="section__container__row__main products">
-                {/* <ProductsData arrayProducts={this.state.db} /> */}
                 <h2 className="products__heading">Products</h2>
-                <div className="products__main">
-                    <ProductsData arrayProducts={this.state.db} />
-                    <div className="products__main__item">
-                        <div className="products__main__item__wrapper">
-                            <div className="products__main__item__wrapper__img">
-                                <a className="img-Change" href="/">
-                                    <img src="http://localhost:3000/../productsImages/adidas1.jpg" alt="item"/>
-                                </a>
-                            </div>
-                            <div className="products__main__item__wrapper__info">
-                                <div className="products__main__item__wrapper__info__price">
-                                    <span>995$</span>
-                                </div>
-                                <div className="products__main__item__wrapper__info__name">
-                                    <span>Nike Vapor</span>
-                                </div>
-                                <div className="products__main__item__wrapper__info__description">
-                                    <span>Кеды мужские Nike Vapor elastic</span>
-                                </div>
-                                <div className="row products__main__item__wrapper__info__link">
-                                    <form method="post" action="/cart/add">
-                                        <input type="hidden" name="id" value="4792929668"/>
-                                        <button className="btn btn-cart" type="submit">
-                                            <i className="fa fa-shopping-cart"></i>
-                                        </button>
-                                    </form>
-                                    <a className="btn" href="/" title="More info">
-                                        <i className="fa fa-info"></i>
-                                    </a>
-                                    <a className="btn quick_view_btn" href="/" title="Quick view">
-                                        <i className="fa fa-eye"></i>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="clearfix"></div>
-                        </div>
-                    </div>
-                </div>
+                <ProductsData arrayProducts={this.state.db} />
             </div>
         )
     }
--- memory/src/Components/sections/main/productData.js
+++ memory/src/Components/sections/main/productData.js
@@ -1,33 +1,49 @@
 import React from 'react';
+import '../../../Styles_Components/Section/main.scss';
 
 class ProductsData extends React.Component {
     render() {
         return(
-            <div>
-                
-                <table className="dataproducts">
-                  <thead>
-                  <tr>
-                      <td>Id</td>
-                      <td>Цена</td>
-                      <td>Бренд</td>
-                      <td>Комментарии</td>
-                  </tr>
-                  </thead>
-
-                  <tbody>
-                    {this.props.arrayProducts.map(
-                    row =>
-                        <tr key="{row.id}">
-                            <td>{row.id}</td>
-                            <td>{row.price}</td>
-                            <td>{row.brand}</td>
-                            <td>{row.description}</td>
-                        </tr>
-                    )}
-                  </tbody>
-              </table>
-            </div>  
+            <div className="products__main">
+                {this.props.arrayProducts.map(row =>
+                    <div className="products__main__item">
+                        <div className="products__main__item__wrapper">
+                            <div className="products__main__item__wrapper__img">
+                                <a className="img-Change" href="/">
+                                    <img src={row.src} alt="item"/>
+                                </a>
+                            </div>
+                            <div className="products__main__item__wrapper__info">
+                                <div className="products__main__item__wrapper__info__price">
+                                    <span>{row.price}</span>
+                                </div>
+                                <div className="products__main__item__wrapper__info__name">
+                                    <span>{row.brand}</span>
+                                </div>
+                                <div className="products__main__item__wrapper__info__description">
+                                    <span>{row.description}</span>
+                                </div>
+                                <div className="row products__main__item__wrapper__info__link">
+                                    <form method="post" action="/cart/add">
+                                        <input type="hidden" name="id" value="4792929668"/>
+                                        <button className="btn btn-cart" type="submit">
+                                            <i className="fa fa-shopping-cart"></i>
+                                        </button>
+                                    </form>
+                                    <a className="btn" href="/" title="More info">
+                                        <i className="fa fa-info"></i>
+                                    </a>
+                                    <a className="btn quick_view_btn" href="/" title="Quick view">
+                                        <i className="fa fa-eye"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="clearfix"></div>
+                        </div>
+                    </div>    
+                )}
+            </div>
+              
             
         )
     }
